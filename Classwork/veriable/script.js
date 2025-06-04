dc43e0d98d8ddfef38d9c5eda50ca9312479f57d
jensipatel369 <jensipatel369@gmail.com>
2025-06-04 11:53:55 +0530

    Prectice

---
+++ Classwork/veriable/script.js
@@ -0,0 +1,44 @@
+// // Variable keyword
+// // 1.Var
+// // 2.Let
+// // 3.Const
+
+// var a = 10;
+// var a = 20; // re-declare, re-intialize
+// var b = 20;
+
+// let c = 30;
+// // let c = 50; re-declaration is not possible
+// c = 45; //cre-intializaion is possible
+
+// const d = 40; // re-declaration and re-intialization both are not possible
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+
+let a = 30;
+let b = 20;
+console.log(a+b);
+
+
+// let num1 = prompt("Enter an number 1 : ");
+// let num2 = prompt("Enter an number 2 : ");
+// alert(num1 - num2);
+
+// let num3 = Number(prompt("Enter an number 3 : "));
+// let num4 = Number(prompt("Enter an number 4 : "));
+// alert(num3 + num4);
+
+// let num5 = Number(prompt("Enter an number 3 : "));
+// let num6 = prompt("Enter an number 4 : ");
+// alert(num5 + num6);
+
+// let num7 = prompt("Enter an number 3 : ");
+// let num8 = Number(prompt("Enter an number 4 : "));
+// alert(num7 + num8);
+
+let first = prompt("Enter your First Name : ");
+let last = prompt("Enter your Last Name : ");
+console.log(first+" "+last);
