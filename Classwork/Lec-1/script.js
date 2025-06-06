e04c093844da429e737ad0f74253f5483f97c016
jensipatel369 <jensipatel369@gmail.com>
2025-06-06 17:13:23 +0530

    Prectice

--- Classwork/Lec-1/script.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello JS")
-console.warn("This is a Warninng")
-console.error("This is a error")
-
-alert("This is an alert")
-confirm("This is a confirmation box")
-prompt("Enter your name : ")
