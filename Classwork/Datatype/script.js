8d9b872a61dc20f2f7f2abd6bc75e4798f5d477b
jensipatel369 <jensipatel369@gmail.com>
2025-06-06 17:22:08 +0530

    Prectice

---
+++ Classwork/Datatype/script.js
@@ -0,0 +1,27 @@
+
+let str = "don't use this";
+let num = 23;
+let bool = true;
+let nullD = null;
+let sym = Symbol("sym");
+// undefined
+// bigint = big intezer
+
+// Print :-
+console.log(typeof(str));
+console.log(typeof(num));
+console.log(typeof(bool));
+console.log(typeof(nullD));
+console.log(typeof(sym));
+
+// let a = prompt("enter a number"); // String
+// let b = confirm("Confirm this or not") // Boolean
+// alert(typeof(b))
+
+let year = prompt("Enter your born year : ");
+alert(2025-year);
+
+// Implicit type conversion
+// Explicit type conversion
+
+
---
+++ Classwork/Template literals/script.js
@@ -0,0 +1,6 @@
+// // Template literals
+// // ``backtick
+
+let naam = "Jensi";
+let str = My name is ${naam};
+console.log(str);
