61e41c73d083870554f2e53778b99246c6f9fd27
jensipatel369 <jensipatel369@gmail.com>
2025-06-12 10:11:53 +0530

    Prectice

--- Classwork/Conditional Statement/Nested if...else/script.js
+++ Classwork/Conditional Statement/Nested if...else/script.js
@@ -1,3 +1,73 @@
-// Nested if..else Statements
+// // Nested if..else Statements
 
-// Question 1 :
+// // Question 1 :
+// // Find a max number
+
+// let a = 45;
+// let b = 45;
+// let c = 20;
+
+// if(a>b){
+//     if(a>c){
+//         alert("a is max")
+//     }else{
+//         alert("c is max")
+//     }
+// }else{
+//     if(b>c){
+//         alert("b is max")
+//     }else{
+//         alert("c is max")
+//     }
+// }
+
+
+// // Question 2 :
+// // Find a min number (By user)
+
+// let x = Number(prompt("Enter first number : "));
+// let y = Number(prompt("Enter second number : "));
+// let z = Number(prompt("Enter third number : "));
+
+// if(x<y){
+//     if(x<z){
+//         alert("x is min")
+//     }else{
+//         alert("z is min")
+//     }
+// }else{
+//     if(y<z){
+//         alert("y is min")
+//     }else{
+//         alert("z is min")
+//     }
+// }
+
+
+// // Question 3 :
+// // Find a two same number
+
+// let a = Number(prompt("Enter first number : "));
+// let b = Number(prompt("Enter second number : "));
+// let c = Number(prompt("Enter third number : "));
+
+// if(a>=b){
+//     if(a==b){
+//         alert(`${a} and ${b} both are equal`)
+//     }else if(a==c){
+//         alert("a and c both are equal")
+//     }else if(a>c){
+//         alert("a is max")
+//     }else{
+//         alert("c is max")
+//     }
+// }else{
+//     if(b==c){
+//         alert("B and c both are same")
+//     }else if(b>c){
+//         alert("b is max")
+//     }else{
+//         alert("c is max")
+//     }
+// }
+
