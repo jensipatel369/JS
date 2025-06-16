c562a2238d4a4cfbfe2d78f53a36535a199506e6
jensipatel369 <jensipatel369@gmail.com>
2025-06-16 11:44:58 +0530

    Prectice

---
+++ Classwork/Loop/script.js
@@ -0,0 +1,49 @@
+// For loop
+// Question 1 :
+// 0 to 4 print
+// for(let i=0; i<5; i++){
+//     console.log(i);    
+// }
+
+// Question 2 :
+// 5 to 1 print
+// for(let i=5; i>0; i--){
+//     console.log(i);    
+// }
+
+// Question 3 :
+// 1 to 50 in even number
+// for(let i=0; i<50; i++){
+//    if(i%2==0){
+//     console.log(i);    
+//    } 
+// }
+
+// Question 4 :
+// Table
+// let num = Number(prompt("Enter your number :"));
+// for(let i=1; i<=10; i++){
+//     // console.log(num + " * " + i + " = " + num*i);   
+//     console.log(`${i} x ${i} = ${i*num}`);   
+// }
+
+
+// Question 5 :
+// 1 to 100 number devide by 7
+// for(let i=1; i<100; i++){
+//    if(i%7==0){
+//     console.log(i);    
+//    } 
+// }
+
+// Question 6 :
+// 1 to 30 square
+// for(let i=1; i<=30; i++){
+//    console.log(`${i} x ${i} = ${i*i}`);   
+// }
+
+// Question 7 :
+// 1 to 30 cube
+for(let i=1; i<=30; i++){
+   console.log(`${i} x ${i} x ${i} = ${i*i*i}`);   
+}
