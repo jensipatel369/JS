6a8662d6125b75c9085a3099773650a04be633e6
jensipatel369 <jensipatel369@gmail.com>
2025-06-22 11:14:38 +0530

    Prectice

---
+++ Classwork/Functions/script.js
@@ -0,0 +1,37 @@
+// Function Decalration
+// function greet(){
+//     console.log("Hello Function");
+// }
+// Function Calling
+// greet();
+
+
+// The value we pass in function paranthese that call Parameter
+// Which value we pass when function calling that call argument
+
+
+// Sum (type 1 : With Parameter (With Argument))
+// let a = Number(prompt("Enter your first number : "))
+// let b = Number(prompt("Enter your second number : "))
+// function sum(a,b){
+//     console.log(a+b);
+// }
+// sum(a,b);
+
+// Sum (type 2 : Without Parameter (Without Argument))
+// let c = Number(prompt("Enter your first number : "))
+// let d = Number(prompt("Enter your second number : "))
+// function sum(){
+//     console.log(c+d);
+// }
+// sum();
+
+// Return Key word :- 
+// function ke andar return ke baad koy bhi write ki hui line ny chlti
+let a = Number(prompt("Enter your first number : "))
+let b = Number(prompt("Enter your second number : "))
+function sum(a,b){
+    return a +b;    
+}
+let result = sum(a,b);
+console.log(result);
