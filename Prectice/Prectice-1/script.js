9f0af946de29047056ad4a03566af5d8fe59e31c
jensipatel369 <jensipatel369@gmail.com>
2025-06-23 11:08:28 +0530

    Prectice

---
+++ Prectice/Prectice-1/script.js
@@ -0,0 +1,14 @@
+for(let i=0;i<=5;i++){
+    for(let j=1;j<=i;j++){
+        document.write(j);
+    }
+    document.write("<br>");
+}
+
+
+for(let i=5;i>0;i--){
+    for(let j=i;j>=1;j--){
+        document.write("*");
+    }
+    document.write("<br>")
+}
