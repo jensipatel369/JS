fe0410e27e165d8d8663ec926150ca8a0458e507
jensipatel369 <jensipatel369@gmail.com>
2025-06-26 11:50:21 +0530

    Prectice

---
+++ Classwork/Array & String/Array/script.js
@@ -0,0 +1,45 @@
+// let arr = [10,20,60,80,50];
+// let arrStr = ["Jensi","Krisha"];
+
+// console.log(arr);
+// console.log(arrStr);
+
+// Question 1 :- 
+// sum of array
+// let arr = [10,12,15,17]
+// let total = 0;
+// for(let i=0; i<=3; i++){
+    // total = total + arr[i];
+    // // total += arr[i];
+// }
+// console.log(total);
+
+
+// Question 2 :- 
+// multiplication of array
+// let arr = [10,12,15,17]
+// let multi = 1;
+// for(let i=0; i<=3; i++){
+//     multi = multi * arr[i];
+// }
+// console.log(multi);
+
+
+// Question 3 :- 
+// square of array
+// let arr = [11,12,13,14]
+// let square = 1;
+// for(let i=0; i<=3; i++){
+//     square = arr[i] * arr[i];
+//     console.log(`${arr[i]} * ${arr[i]} = `,square);
+// }
+
+
+// Question 4 :- 
+// cube of array
+let arr = [11,12,13,14]
+let cube = 1;
+for(let i=0; i<=3; i++){
+    cube = arr[i] * arr[i] * arr[i];
+    console.log(`${arr[i]} * ${arr[i]} * ${arr[i]} = `,cube);
+}
