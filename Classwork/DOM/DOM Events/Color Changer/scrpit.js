653fc98631b4096384196beca13ca07f80c8c207
jensipatel369 <jensipatel369@gmail.com>
2025-06-30 11:38:57 +0530

    Prectice

--- Classwork/DOM/DOM Events/Color Changer/scrpit.js
+++ Classwork/DOM/DOM Events/Color Changer/scrpit.js
@@ -1,15 +1,27 @@
 let box = document.querySelector("#box");
 let btn = document.querySelector("#btn");
 
-let arr = ["black","green","red","Brown","yellow"]
+let arr = ["black","green","red","Brown","yellow","Pink"]
 
 let i = 0;
 
 function magic(){
     box.style.backgroundColor = arr[i];
+    box.innerHTML = arr[i];
     if(i<arr.length){
         i += 1;
     }else{
         i = 0;
     }
 }
+
+// The forEach loop use to iterate the array, not return the new array
+arr.forEach((item)=>{
+    console.log(item);    
+})
+
+// The forEach loop use to iterate the array, return the new array
+let arr2 = arr.map((item)=>{
+    return item + " map";
+})
+console.log(arr2);
