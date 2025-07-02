486f9bfdb3f8abcc6bd7f1e67ff48125952b6f17
jensipatel369 <jensipatel369@gmail.com>
2025-07-02 11:01:51 +0530

    Prectice

--- Prectice/Image Slider/scrpit.js
+++ Prectice/Image Slider/scrpit.js
@@ -1,7 +1,7 @@
-let left = document.querySelector("#left");
-let right = document.querySelector("#right");
+// let left = document.querySelector("#left");
+// let right = document.querySelector("#right");
 
-let arr = ["url('https://images.pexels.com/photos/936194/pexels-photo-936194.jpeg'),url('https://images.unsplash.com/photo-1526779259212-939e64788e3c?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D')"]
+// let arr = ["url('https://images.pexels.com/photos/936194/pexels-photo-936194.jpeg'),url('https://images.unsplash.com/photo-1526779259212-939e64788e3c?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D')"]
 
 // let i = 0;
 
