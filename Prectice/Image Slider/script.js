46a261a8efcb37918cfb18406e3ae13df5e1fa93
jensipatel369 <jensipatel369@gmail.com>
2025-07-02 11:32:28 +0530

    Prectice

--- Prectice/Image Slider/script.js
+++ Prectice/Image Slider/script.js
@@ -1,7 +1,7 @@
-let img = document.querySelector("#img");
-let btn = document.querySelector("#btn");
+let img = document.querySelector("img");
+let btn = document.querySelector("btn");
 
-let arr = ["1.png","2.png","3.png"];
+let arr = ["1.png","2.png"];
 
 let i = 0;
 
