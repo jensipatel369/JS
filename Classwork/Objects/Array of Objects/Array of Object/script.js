a80d4ea7ed15c0b08163bb5e0a6c817e33159aea
jensipatel369 <jensipatel369@gmail.com>
2025-07-04 11:49:14 +0530

    Prectice

--- Classwork/Objects/Array of Objects/Array of Object/script.js
+++ Classwork/Objects/Array of Objects/Array of Object/script.js
@@ -22,3 +22,27 @@ let class11th = [
         rollNumber : 1005
     }
 ]
+
+// For Each loop naya data return nhi krta...
+// item Function name hai...
+class11th.forEach((item)=>{
+    console.log(item.name);
+    console.log(item.rollNumber);    
+})
+
+// For Each loop naya data return krta hai...
+class11th.map((item)=>{
+    if(item.rollNumber == 1003){
+        console.log(item.name);        
+    }
+})
+
+let confirmedStudent = class11th.map((item)=>{
+    if(item.rollNumber == 1002){
+        console.log(item.name);   
+        return item;     
+    }
+})
+
+let singleData = class11th.find((item)=>item.rollNumber == 1004);
+console.log(singleData);
